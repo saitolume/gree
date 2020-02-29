265b3e70e488dbaace60cf449e91cccc3a33bdee
Tadao Iseki <saitoeku3@gmail.com>
2020-02-29 14:16:50 +0900

    Fix to read .gitignore in selected dir

--- src/reader.rs
+++ src/reader.rs
@@ -1,13 +1,25 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::fs;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-pub fn read_ignore() -> Result<Vec<String>, Error> {
+pub fn read_ignore(root_path: &str) -> Vec<String> {
     let mut ignore_file_names: Vec<String> = vec![];
+    let gitignore_path = match root_path {
+        ".." | "." => "/.gitignore",
+        _ => ".gitignore",
+    };
 
-    for line in BufReader::new(File::open(".gitignore")?).lines() {
-        let file_name = Path::new(&line?).file_name().unwrap().to_str().unwrap().to_string();
-        ignore_file_names.push(file_name);
+    if let Ok(file) = fs::File::open(Path::new(&(root_path.to_owned() + gitignore_path))) {
+        for line in BufReader::new(file).lines() {
+            let file_name = Path::new(&line.expect(""))
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+            ignore_file_names.push(file_name);
+        }
     }
-    Ok(ignore_file_names)
+
+    ignore_file_names
 }
